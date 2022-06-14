58e2fe9cb8fa71ecf6ad5788c69a1a6f080843db
root <marisettymadhu16@gmail.com>
2022-06-14 07:26:11 +0000

    authentication-and-authorization

---
+++ src/components/AllProductsSection/index.js
@@ -0,0 +1,59 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import ProductCard from '../ProductCard'
+import './index.css'
+
+class AllProductsSection extends Component {
+  state = {
+    productsList: [],
+  }
+
+  componentDidMount() {
+    this.getPoducts()
+  }
+
+  getPoducts = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiURL = 'https://apis.ccbp.in/products'
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiURL, options)
+    if (response.ok === true) {
+      const fetchData = await response.json()
+      console.log(fetchData)
+      const updateData = fetchData.products.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({productsList: updateData})
+    }
+  }
+
+  renderProductsList = () => {
+    const {productsList} = this.state
+    return (
+      <div>
+        <h1 className="products-list-heading">All Products</h1>
+        <ul className="products-list">
+          {productsList.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return <>{this.renderProductsList()}</>
+  }
+}
+
+export default AllProductsSection
